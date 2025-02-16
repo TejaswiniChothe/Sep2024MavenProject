2921f5a414fee4f2d72e0edca04635381c624cee
TejaswiniChothe <tejaswini.chothe11@gmail.com>
2025-02-16 14:45:30 +0530

    Hello java page chnages pushed

---
+++ src/main/java/SeleniumSession/HelloJava.java
@@ -0,0 +1,11 @@
+package SeleniumSession;
+
+public class HelloJava {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.err.println("Hello java");
+	}
+
+}
